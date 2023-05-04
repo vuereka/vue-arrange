a5a1159b60e69a289c1e422e1d8c8b654c780e90
Frido Emans <frido@ipfs-search.com>
2023-05-04 22:19:51 +0200

    example build

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,35 @@
-import path from "path";
+import {resolve} from "path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, "src/index.ts"),
-      name: "vue-arrange",
-      fileName: (format) => `vue-arrange.${format}.js`,
-    },
-    rollupOptions: {
-      external: ["vue"],
-      output: {
-        // Provide global variables to use in the UMD build
-        // Add external deps here
-        globals: {
-          vue: "Vue",
+export default defineConfig(({mode}) => {
+  if (mode === 'example') {
+    return {
+      base: './',
+      build: {
+        outDir: 'build'
+      },
+      plugins: [vue()],
+    }
+  }
+  else return {
+    build: {
+      lib: {
+        entry: resolve(__dirname, "src/index.ts"),
+        name: "vue-arrange",
+        fileName: (format) => `vue-arrange.${format}.js`,
+      },
+      rollupOptions: {
+        external: ["vue"],
+        output: {
+          // Provide global variables to use in the UMD build
+          // Add external deps here
+          globals: {
+            vue: "Vue",
+          },
         },
       },
     },
-  },
-  plugins: [vue()],
-});
+    plugins: [vue()],
+}});
