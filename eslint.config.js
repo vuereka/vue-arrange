5bd53fde79ce4a38bb0c3c11427566c4c4241b0c
Frido Emans <frido@ipfs-search.com>
2024-11-17 15:39:00 +0100

    fix eslint config file

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,6 @@ import tseslint from "typescript-eslint";
 import pluginVue from "eslint-plugin-vue";
 
 
-/** @type {import('eslint').Linter.Config[]} */
 export default [
   {files: ["**/*.{js,mjs,cjs,ts,vue}"]},
   {languageOptions: { globals: globals.browser }},
